1e0f82cfa926b332e6c0c68ca6b5fa8a1dc51a8e
neha-bhatla <nehab2708@gmail.com>
2024-07-28 07:30:31 -0400

    update

--- src/app/logmood/page.tsx
+++ src/app/logmood/page.tsx
@@ -9,70 +9,87 @@ import ThumbUpOffAltOutlinedIcon from '@mui/icons-material/ThumbUpOffAltOutlined
 import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutlined';
 import { Box } from "@mui/material";
 import Typography from '@mui/material/Typography';
-import {CheckboxGroup, Checkbox} from "@nextui-org/checkbox";
+import { CheckboxGroup, Checkbox } from "@nextui-org/checkbox";
+import { Textarea } from "@nextui-org/input";
+import { Button } from "@nextui-org/button";
 
 export default function LogMood() {
   return (
-    <div>
-      <Box sx={{
-          position: 'absolute',
-          top: '10vh',
-          left: '50%',
-          transform: 'translateX(-50%)',
-          display: 'flex',
-          flexDirection: 'column', 
-          alignItems: 'center',
-          gap: '260px',
-      }}>
+    <center>
+      <div>
+        <Box sx={{ margin: 'auto 400px', marginTop: '50px', marginBottom: '50px', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Box sx={{display: 'flex', flexDirection: 'row', gap: '50px'
+        }}>
         
+        <Button color="secondary" >
+            Back
+          </Button>
 
-        <Typography variant="h4" sx={{ fontWeight: "bold"}}>
-          how are you feeling today?
-        </Typography>
+          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: '110px' }}>
+            how are you feeling today?
+          </Typography>
 
-        <Box sx={{
-            position: 'absolute',
-            top: '20vh',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: '180px',
-        }}>
-          <SentimentVerySatisfiedOutlinedIcon style={{ transform: 'scale(7)' }} />
-          <SentimentSatisfiedOutlinedIcon style={{ transform: 'scale(7)' }} />
-          <SentimentNeutralOutlinedIcon style={{ transform: 'scale(7)' }} />
-          <SentimentDissatisfiedOutlinedIcon style={{ transform: 'scale(7)' }} />
-          <SickOutlinedIcon style={{ transform: 'scale(7)' }} />
+          </Box>
+          
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: '180px', marginBottom: '110px' }}>
+            <button>
+              <SentimentVerySatisfiedOutlinedIcon style={{ transform: 'scale(7)' }} />
+            </button>
+            <button>
+              <SentimentSatisfiedOutlinedIcon style={{ transform: 'scale(7)' }} />
+            </button>
+            <button>
+              <SentimentNeutralOutlinedIcon style={{ transform: 'scale(7)' }} />
+            </button>
+            <button>
+              <SentimentDissatisfiedOutlinedIcon style={{ transform: 'scale(7)' }} />
+            </button>
+            <button>
+              <SickOutlinedIcon style={{ transform: 'scale(7)' }} />
+            </button>
+          </Box>
+
+          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: '110px' }}>
+            were you kind to yourself today?
+          </Typography>
+          
+          <Box sx={{ display: 'flex', flexDirection: 'row', gap: '180px', marginBottom: '110px' }}>
+            <button>
+              <ThumbUpOffAltOutlinedIcon style={{ transform: 'scale(7)' }} />
+            </button>
+            <button>
+              <ThumbDownOffAltOutlinedIcon style={{ transform: 'scale(7)' }} />
+            </button>
+          </Box>
+
+          <Typography variant="h4" sx={{ fontWeight: "bold", marginBottom: '85px' }}>
+            what made you feel this way today?
+          </Typography>
+          
+          <Box sx={{ marginBottom: '110px' }}>
+            <CheckboxGroup style={{ transform: 'scale(1.5)' }}>
+              <Checkbox value="family">friends</Checkbox>
+              <Checkbox value="work">work</Checkbox>
+              <Checkbox value="school">school</Checkbox>
+              <Checkbox value="friends">friends</Checkbox>
+              <Checkbox value="activities">activities</Checkbox>
+              <Checkbox value="no reason">no reason, I just feel like this</Checkbox>
+            </CheckboxGroup>
+          </Box>
+
+          <Textarea 
+            label="let's journal! ✍🏼"
+            placeholder="start an entry"
+            size="lg"
+            style={{ marginBottom: '50px' }} 
+          />
+          
+          <Button color="primary" style={{ marginTop: '30px' }} >
+            Submit
+          </Button>
+          
         </Box>
-
-        <Typography variant="h4" sx={{ fontWeight: "bold"}}>
-          were you kind to yourself today?
-        </Typography>
-
-        <Box sx={{
-            position: 'absolute',
-            top: '55vh',
-            left: '50%',
-            transform: 'translateX(-50%)',
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            gap: '180px',
-        }}>
-          <ThumbUpOffAltOutlinedIcon style={{ transform: 'scale(7)' }} />
-          <ThumbDownOffAltOutlinedIcon style={{ transform: 'scale(7)' }} />
-        </Box>
-        <CheckboxGroup style={{ transform: 'scale(1.5)' }}>
-          <Checkbox value="family" >friends</Checkbox>
-          <Checkbox value="work" >work</Checkbox>
-          <Checkbox value="school" >school</Checkbox>
-          <Checkbox value="friends" >friends</Checkbox>
-          <Checkbox value="activies" >activies</Checkbox>
-          <Checkbox value="no reason">no reason, I just feel like this</Checkbox>
-        </CheckboxGroup>
-      </Box>
-    </div>
+      </div>
+    </center>
   );
 }
